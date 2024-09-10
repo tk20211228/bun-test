7e39413451dc624de0addcbf65b20a03a565ef8a
tk20211228 <t3kuboki@gmail.com>
2024-09-11 06:46:05 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <div>test</div>
+          <div>test for mac</div>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
